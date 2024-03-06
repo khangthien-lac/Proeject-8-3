e353061254f4ece35946bca259ef1a9cda39bc3a
khangthien-lac <161085032+khangthien-lac@users.noreply.github.com>
2024-03-06 11:51:44 +0700

    Update project.py

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ import os
 import streamlit as st
 from PIL import ImageGrab
 
-st.title('Demo web')
+st.title('Chao mung cac ban den voi trang web cua minh')
 path = '.\BanHoc'
 images = []
 classNames = []
@@ -25,37 +25,41 @@ def findEncodings(images):
     return encodeList
  
 encodeListKnown = findEncodings(images)
- 
-cap = st.camera_input('Take a picture')
- 
-while True:
-    success, img = cap.read()
-    # img = captureScreen()
-    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
-    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
-    
-    facesCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
- 
-    for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
-        matchIndex = np.argmin(faceDis)
-        
-        if matches[matchIndex]:
-            name = classNames[matchIndex].upper()
-            #print(name)
-            y1,x2,y2,x1 = faceLoc
-            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
-            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
-            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
- 
-    cv2.imshow('Webcam',img)
-    c = cv2.waitKey(1)
-    if c == ord('q'): 
-        break
 
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+cap = cv2.VideoCapture(0)
+
+def main(classNames,Run,FRAME_WINDOW):
+    while Run:
+        _, img = cap.read()
+        FRAME_WINDOW.image(img)
+        imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+        
+        facesCurFrame = face_recognition.face_locations(imgS)
+        encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+    
+        for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+            matchIndex = np.argmin(faceDis)
+            
+            if matches[matchIndex]:
+                name = classNames[matchIndex].upper()
+                y1,x2,y2,x1 = faceLoc
+                y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+                cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+                cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+                cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+    
+        cv2.imshow('Webcam',img)
+        # c = cv2.waitKey(1)
+        # if c == ord('q'): 
+        #     break
+    else: 
+        st.write('Stopped')
+
+main(classNames)
 cap.release()
 cv2.destroyAllWindows()
-st.write(f'{path}')
